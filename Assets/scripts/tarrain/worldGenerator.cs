c4e6c4b44d42bb74a5c32fd1ae5e946717a1e8f1
Majkarto <majkarto@gmail.com>
2024-03-15 16:11:00 +0100

    0.0.1
    +first chunk prototype

--- Assets/scripts/tarrain/worldGenerator.cs
+++ Assets/scripts/tarrain/worldGenerator.cs
@@ -1,73 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class worldGenerator : MonoBehaviour
 {
-	Mesh mesh;
+    // Deklaracja czterowymiarowej tablicy
+    int[,,] chunkData = new int[16, 16, 256];
 
-	Vector3[] vertices;
-	int[] triangles;
+    // Przyk³adowe u¿ycie tablicy - ustawienie wartoœci dla konkretnego bloku
+    private int xPos, yPos, zPos, idBlock, localHeight;
+    public GameObject blockPrefab;
+    private void Start()
+    {
 
-	public int xSize = 20;
-	public int zSize = 20;
+        for (int x = 0; x <= 16; x++)
+        {
+            for (int z = 0; z <= 16; z++)
+            {
+                localHeight = Random.Range(4, 8);
+                for (int y = 0; y < localHeight; y++)
+                {
+                    xPos = x;
+                    zPos = z;
+                    yPos = y;
+                    chunkData[xPos, yPos, zPos] = 1;
+                    Vector3 spawnPosition = transform.position + new Vector3(xPos - 8, yPos, zPos - 8);
+                    Instantiate(blockPrefab, spawnPosition, Quaternion.identity);
+                }
 
-	public float strength = 0.3f;
-
-	void Start()
-	{
-		mesh = new Mesh();
-		GetComponent<MeshFilter>().mesh = mesh;
-
-		CreateShape();
-		UpdateMesh();
-	}
-
-	void CreateShape()
-	{
-		vertices = new Vector3[(xSize + 1) * (zSize + 1)];
-
-
-		for (int i = 0, z = 0; z <= zSize; z++)
-		{
-			for (int x = 0; x <= xSize; x++)
-			{
-				float y = Mathf.PerlinNoise(x * strength, z * strength) * 2f;
-				vertices[i] = new Vector3(x, y, z);
-				i++;
-			}
-		}
-
-		triangles = new int[xSize * zSize * 6];
-
-		int vert = 0;
-		int tris = 0;
-
-		for (int z = 0; z < zSize; z++)
-		{
-			for (int x = 0; x < xSize; x++)
-			{
-				triangles[tris + 0] = vert + 0;
-				triangles[tris + 1] = vert + xSize + 1;
-				triangles[tris + 2] = vert + 1;
-				triangles[tris + 3] = vert + 1;
-				triangles[tris + 4] = vert + xSize + 1;
-				triangles[tris + 5] = vert + xSize + 2;
-
-				vert++;
-				tris += 6;
-			}
-			vert++;
-		}
-	}
-
-	void UpdateMesh()
-	{
-		mesh.Clear();
-
-		mesh.vertices = vertices;
-		mesh.triangles = triangles;
-
-		mesh.RecalculateNormals();
-	}
+            }
+        }
+        Debug.Log(chunkData);
+    }
 }
